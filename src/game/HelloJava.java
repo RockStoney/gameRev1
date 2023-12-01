157d31cd8311055bae90d569840926c01b6d52b6
painN <slavikrat@gmail.com>
2023-12-01 17:36:48 +0200

    Second commit

--- src/game/HelloJava.java
+++ src/game/HelloJava.java
@@ -3,5 +3,6 @@ package game;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello game project");
+        System.out.println("Hello game");
     }
 }
