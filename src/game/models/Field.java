e0cb8f6eee16b7d668ecf2e9f421a53aeadf466a
painN <slavikrat@gmail.com>
2023-12-04 16:04:08 +0200

    Rewrite and comment

--- src/game/models/Field.java
+++ src/game/models/Field.java
@@ -2,11 +2,12 @@ package game.models;
 
 public class Field {
     private final int SIZE_FIELD;
-    private String[][] stateField;
+    final private String[][] stateField;
     private final String DEFAULT_SYMBOL = " ";
     private final int MIN_VALUE;
     private final int MAX_VALUE;
 
+    // Constructor
     public Field(int SIZE_FIELD) {
         this.SIZE_FIELD = SIZE_FIELD;
         this.MIN_VALUE = 0;
@@ -15,20 +16,31 @@ public class Field {
         setStateField(DEFAULT_SYMBOL);
     }
 
+    // Returns field's size
     public int getSIZE_FIELD() {return SIZE_FIELD;}
+
+    // Returns field's state
     public String[][] getStateField() {return stateField;}
+
+    // Returns default symbol
     public String getDEFAULT_SYMBOL() {return DEFAULT_SYMBOL;}
+
+    // Returns minimal value
     public int getMIN_VALUE() {return MIN_VALUE;}
+
+    // Returns maximal value
     public int getMAX_VALUE() {return MAX_VALUE;}
 
+    // Sets field's state
     public void setStateField(String symbol) {
         for (int i = 0; i < getSIZE_FIELD(); i++) {
             for (int j = 0; j < getSIZE_FIELD(); j++) {
                 setCellField(i, j, symbol);
             }
         }
-        this.stateField = stateField;
     }
+
+    // Checks coordinate
     public boolean checkCoordinate (int coordinate){
         if (coordinate >= getMIN_VALUE() && coordinate < getMAX_VALUE()){
             return true;
@@ -37,18 +49,22 @@ public class Field {
             return false;
         }
     }
+
+    // Returns field's sign
     public String getCellField (int i, int j){
         if (checkCoordinate(i) && checkCoordinate(j)) {
             return stateField[i][j];
         }
         return null;
     }
+
+    // Sets field's sign
     public void setCellField(int i, int j, String symbol) {
         if (checkCoordinate(i) && checkCoordinate(j)){
-            if (stateField[i][j] == getDEFAULT_SYMBOL() || stateField[i][j] == null){
+            if (stateField[i][j] == getDEFAULT_SYMBOL() || stateField[i][j] == null) {
                 this.stateField[i][j] = symbol;
             } else {
-                System.out.println("This field occupied");
+                System.out.println("This field occupied. Try again");
             }
         }
     }
